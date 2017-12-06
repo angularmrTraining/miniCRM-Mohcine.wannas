e4f7d2fd1f772cd5d0f3359fed409b93f5843fbf
a.salah <salah.abdeltif@gmail.com>
2017-12-06 17:31:35 +0100

    migration vers angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { BrowserModule,  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ClarityModule } from 'clarity-angular';
 import { AppRoutingModule } from './app-routing.module';
@@ -27,7 +26,6 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     FormsModule,
     HttpModule,
     ClarityModule.forRoot(),
-    BrowserAnimationsModule,
     AppRoutingModule,
     LoginModule,
     AdminModule
